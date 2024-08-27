a4ee1b7acad59f3334f644435bbca816d12e7e2a
prince bassey <basseyprince75@gmail.com>
2024-08-27 18:26:11 +0100

    modified package.json file and added default route

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.use(morgan("combined"));
 
 // Middleware to parse incoming request bodies
 app.use(express.json());
-app.get("/", (res, res) => {
-  res.send("Welcome to Her Health");
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "WELCOME TO HER HEALTH MANAGEMENT SYSTEM API",
+  });
 });
 app.use("/api/v1", routes);
 require("./database/database").connectDB();
